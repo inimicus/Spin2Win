218565f1d0ea18a2f84f940a3d46bb7a0b1b4b9b
Garret Jennings <gjennings@me.com>
2018-08-23 17:50:48 -0700

    Whitespace.

--- src/Tracking.lua
+++ src/Tracking.lua
@@ -20,21 +20,21 @@ function S2W.Tracking.RegisterEvents()
     -- SPINNING ------------------------
     EVENT_MANAGER:RegisterForEvent(S2W.name, EVENT_EFFECT_CHANGED, S2W.Tracking.DidSpin)
     EVENT_MANAGER:AddFilterForEvent(S2W.name, EVENT_EFFECT_CHANGED,
-		REGISTER_FILTER_ABILITY_ID,					SPIN2WIN_EFFECT_ID,
-		REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE,	COMBAT_UNIT_TYPE_PLAYER)
+        REGISTER_FILTER_ABILITY_ID,                 SPIN2WIN_EFFECT_ID,
+        REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE,    COMBAT_UNIT_TYPE_PLAYER)
 
-	-- WINNING ---------------------------------------------------------------
-	-- Alliance vs Alliance
-	EVENT_MANAGER:RegisterForEvent(S2W.name, EVENT_COMBAT_EVENT, _AvAWin)
+    -- WINNING ---------------------------------------------------------------
+    -- Alliance vs Alliance
+    EVENT_MANAGER:RegisterForEvent(S2W.name, EVENT_COMBAT_EVENT, _AvAWin)
     EVENT_MANAGER:AddFilterForEvent(S2W.name, EVENT_COMBAT_EVENT,
-		REGISTER_FILTER_ABILITY_ID,		SPIN2WIN_ABILITY_ID,
-		REGISTER_FILTER_UNIT_TAG,		COMBAT_UNIT_TYPE_PLAYER,
-		REGISTER_FILTER_IS_ERROR,		false,
-		REGISTER_FILTER_COMBAT_RESULT,	ACTION_RESULT_KILLING_BLOW)
+        REGISTER_FILTER_ABILITY_ID,     SPIN2WIN_ABILITY_ID,
+        REGISTER_FILTER_UNIT_TAG,       COMBAT_UNIT_TYPE_PLAYER,
+        REGISTER_FILTER_IS_ERROR,       false,
+        REGISTER_FILTER_COMBAT_RESULT,  ACTION_RESULT_KILLING_BLOW)
 
-	-- Battlegrounds
-	-- This needs further testing
-	EVENT_MANAGER:RegisterForEvent(S2W.name, EVENT_BATTLEGROUND_KILL, _BGWin)
+    -- Battlegrounds
+    -- This needs further testing
+    EVENT_MANAGER:RegisterForEvent(S2W.name, EVENT_BATTLEGROUND_KILL, _BGWin)
     EVENT_MANAGER:AddFilterForEvent(S2W.name, EVENT_BATTLEGROUND_KILL,
         --REGISTER_FILTER_ABILITY_ID,   SPIN2WIN_ABILITY_ID,
         REGISTER_FILTER_UNIT_TAG,   COMBAT_UNIT_TYPE_PLAYER)
@@ -42,58 +42,58 @@ function S2W.Tracking.RegisterEvents()
 end
 
 function S2W.Tracking.UnregisterEvents()
-	EVENT_MANAGER:UnregisterForEvent(S2W.name, EVENT_EFFECT_CHANGED)
+    EVENT_MANAGER:UnregisterForEvent(S2W.name, EVENT_EFFECT_CHANGED)
     EVENT_MANAGER:UnregisterForEvent(S2W.name, EVENT_COMBAT_EVENT)
     EVENT_MANAGER:UnregisterForEvent(S2W.name, EVENT_BATTLEGROUND_KILL)
-	S2W:Trace(2, "Unregistering effects")
+    S2W:Trace(2, "Unregistering effects")
 end
 
 function S2W.Tracking.DidSpin(_, changeType, _, effectName, unitTag, _, _,
         stackCount, _, _, _, _, _, _, _, effectAbilityId)
 
-	if changeType ~= EFFECT_RESULT_GAINED then return end
+    if changeType ~= EFFECT_RESULT_GAINED then return end
 
-	S2W.Tracking.SpinCount = S2W.Tracking.SpinCount + 1
-	S2W:Trace(2, zo_strformat("Counted: <<1>> spins", S2W.Tracking.SpinCount))
+    S2W.Tracking.SpinCount = S2W.Tracking.SpinCount + 1
+    S2W:Trace(2, zo_strformat("Counted: <<1>> spins", S2W.Tracking.SpinCount))
 
-	S2W.UI.UpdateSpins(S2W.Tracking.SpinCount)
+    S2W.UI.UpdateSpins(S2W.Tracking.SpinCount)
 
 end
 
 --EVENT_COMBAT_EVENT (
---	number eventCode,
---	ActionResult result,
---	boolean isError,
---	string abilityName,
---	number abilityGraphic,
---	ActionSlotType abilityActionSlotType,
---	string sourceName,
---	CombatUnitType sourceType,
---	string targetName,
---	CombatUnitType targetType,
---	number hitValue,
---	CombatMechanicType powerType,
---	DamageType damageType,
---	boolean log,
---	number sourceUnitId,
---	number targetUnitId,
---	number abilityId
+--  number eventCode,
+--  ActionResult result,
+--  boolean isError,
+--  string abilityName,
+--  number abilityGraphic,
+--  ActionSlotType abilityActionSlotType,
+--  string sourceName,
+--  CombatUnitType sourceType,
+--  string targetName,
+--  CombatUnitType targetType,
+--  number hitValue,
+--  CombatMechanicType powerType,
+--  DamageType damageType,
+--  boolean log,
+--  number sourceUnitId,
+--  number targetUnitId,
+--  number abilityId
 --)
 function _AvAWin(eventID, result, isError, abilityName, _, _, sourceName, sourceType, targetName, targetType, _, _, _, _, _, _, abilityId)
-	S2W:Trace(2, zo_strformat("Win: <<1>> (<<2>>) on target <<3>> (<<4>>) with result <<5>>", abilityName, abilityId, targetName, targetType, result))
-	S2W.Tracking.DidWin()
+    S2W:Trace(2, zo_strformat("Win: <<1>> (<<2>>) on target <<3>> (<<4>>) with result <<5>>", abilityName, abilityId, targetName, targetType, result))
+    S2W.Tracking.DidWin()
 end
 
 --EVENT_BATTLEGROUND_KILL (
---	number eventCode,
---	string killedPlayerCharacterName,
---	string killedPlayerDisplayName,
---	BattlegroundAlliance killedPlayerBattlegroundAlliance,
---	string killingPlayerCharacterName,
---	string killingPlayerDisplayName,
---	BattlegroundAlliance killingPlayerBattlegroundAlliance,
---	BattlegroundKillType battlegroundKillType,
---	number killingAbilityId
+--  number eventCode,
+--  string killedPlayerCharacterName,
+--  string killedPlayerDisplayName,
+--  BattlegroundAlliance killedPlayerBattlegroundAlliance,
+--  string killingPlayerCharacterName,
+--  string killingPlayerDisplayName,
+--  BattlegroundAlliance killingPlayerBattlegroundAlliance,
+--  BattlegroundKillType battlegroundKillType,
+--  number killingAbilityId
 --)
 function _BGWin(_, killedPlayerCharacterName, killedPlayerDisplayName, _, killingPlayerCharacterName, _, _, battlegroundKillType, killingAbilityId)
     -- Ignore non-spin kills
@@ -103,8 +103,8 @@ function _BGWin(_, killedPlayerCharacterName, killedPlayerDisplayName, _, killin
 end
 
 function S2W.Tracking.DidWin()
-	S2W:Trace(1, "Counted " .. S2W.Tracking.WinCount .. " wins")
-	S2W.Tracking.WinCount = S2W.Tracking.WinCount + 1
-	S2W.UI.UpdateWins(S2W.Tracking.WinCount)
+    S2W:Trace(1, "Counted " .. S2W.Tracking.WinCount .. " wins")
+    S2W.Tracking.WinCount = S2W.Tracking.WinCount + 1
+    S2W.UI.UpdateWins(S2W.Tracking.WinCount)
 end
 
