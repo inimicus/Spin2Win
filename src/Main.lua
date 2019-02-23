b597d2be085268ef36f70b4fdf66c78f30a6f92c
Garret Jennings <gjennings@me.com>
2019-02-23 14:13:53 -0800

    Bump to version 1.2.1

--- src/Main.lua
+++ src/Main.lua
@@ -8,7 +8,7 @@
 
 S2W             = {}
 S2W.name        = "Spin2Win"
-S2W.version     = "1.2.0"
+S2W.version     = "1.2.1"
 S2W.dbVersion   = 1
 S2W.slash       = "/s2w"
 S2W.prefix      = "[Spin2Win] "
